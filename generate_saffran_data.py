0546abea5b3c1d80547ddcb6a70b6009dfdc10d8
Shinjini Ghosh <shinghos@mit.edu>
2020-12-12 22:08:47 -0500

    generating data

---
+++ generate_saffran_data.py
@@ -0,0 +1,12 @@
+"""
+Artificially generate Saffran's data
+"""
+
+word_list = ["pabiku", "tiduko", "golatu", "daropi"]
+syllable_list = [] #TODO
+
+speech_stream = [] #randomly take words from word list, split into syllables (just take 2 letters each), concatenate
+
+# use induce pcfg to get grammar
+
+# parse speech stream on grammar
